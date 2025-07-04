9f7fbdf34f95bedd01ef18027f3ce6dfcd22e4a6
Gursehaj Singh <gursehajkamboj@gmail.com>
2025-07-03 23:25:28 -0400

    Updating NPC Behaviour System

--- Assets/NpcSchedules/ActionsScripts/GoToDestination.cs
+++ Assets/NpcSchedules/ActionsScripts/GoToDestination.cs
@@ -1,12 +1,28 @@
 using UnityEngine;
-using KambojGames.Utilities2D;
+using KambojGames.Pathfinding2D;
+using KambojGames.DialogueSystem;
 
 public class GoToDestination : NpcAction
 {
     [Tooltip("Starting position for npc, if any")]
     [SerializeField] Vector2 m_startingPosition;
+    [SerializeField] Vector2 m_destinationPosition;
+
+    [Header("PathFinding")]
+    [SerializeField] float m_npcMoveSpeed = 1.0f;
+    [SerializeField] float m_smothingfactor = 2.5f;
+    [SerializeField] float m_distBetweenWayPoints = 0.4f;
+    [SerializeField] float m_pathUpdateInterval = 0.5f;
+
+    [Header("Dialogues")]
+    [SerializeField] private ConditionalDialogue m_actionDialogue;
+
     [Header("Game Event")]
     [SerializeField]GameEvent m_actionToDoWhenReachedDestination;
+
+    private AIPathfinding2D m_pathFinding;
+    private DialogueInteraction m_dI;
+    NpcBehaviourControl m_controller;
 
     public override void Initialize(Rigidbody2D rb, NpcTaskMediatorSO mediator)
     {
@@ -15,11 +31,38 @@ public class GoToDestination : NpcAction
         {
             Rb.transform.position = m_startingPosition;
         }
+
+        m_pathFinding = new AIPathfinding2D();
+        m_pathFinding.InitializePath(Rb, m_destinationPosition, m_smothingfactor, m_distBetweenWayPoints, m_pathUpdateInterval);
+        m_controller = rb.GetComponent<NpcBehaviourControl>();
+        m_dI = rb.GetComponentInChildren<DialogueInteraction>();
+        m_dI.Initialzie();
+        m_dI.AddDialogueObject(m_actionDialogue.GetDialogue());
     }
     public override void DoAction()
     {
         base.DoAction();
-        m_actionToDoWhenReachedDestination?.Raise();
-        IsActionCompleted = true;
+
+        if (m_pathFinding != null)
+        {
+            if (m_pathFinding.isPathCompleted())
+            {
+                m_dI.ClearDialogueObject();
+                Rb.velocity = Vector2.zero;
+                m_pathFinding = null;
+                IsActionCompleted = true;
+                m_dI = null;
+                m_controller = null;
+                m_actionToDoWhenReachedDestination?.Raise();
+               
+            }
+            else
+            {
+                m_pathFinding.MoveToPath(m_destinationPosition, m_npcMoveSpeed);
+            }
+        }
+     
+       
+        
     }
 }
--- Assets/NpcSchedules/ActionsScripts/TeleportToDestination.cs
+++ Assets/NpcSchedules/ActionsScripts/TeleportToDestination.cs
@@ -1,29 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using KambojGames.Utilities2D;
 
 public class TeleportToDestination : NpcAction
 {
     [SerializeField] Vector3 m_teleportDestination;
     [SerializeField] string m_sortingLayerName;
     [SerializeField] int m_sortingLayerOrder;
+    [SerializeField] Vector2 m_moveVectorAfterTeleportation;
+
+    private SpriteRenderer m_sprite;
+    private bool m_teleported;
 
     public override void Initialize(Rigidbody2D rb, NpcTaskMediatorSO mediator)
     {
         base.Initialize(rb, mediator);
+        rb.transform.position = m_teleportDestination;
+        Character character = rb?.GetComponent<Character>();
+        m_sprite = Rb?.GetComponentInChildren<SpriteRenderer>();
+        m_teleported = false;
+
+        if (character)
+        {
+            character.MoveVector = m_moveVectorAfterTeleportation;
+        }
     }
 
     public override void DoAction()
     {
-        if (Rb.transform.position != m_teleportDestination) return;
         base.DoAction();
-        Rb.transform.position = m_teleportDestination;
-        SpriteRenderer sprite = Rb?.GetComponentInChildren<SpriteRenderer>();
-        if(sprite)
-        {
-            sprite.sortingLayerName = m_sortingLayerName;
-            sprite.sortingOrder = m_sortingLayerOrder;
-        }
-     
+        if (m_teleported) return;
+        m_sprite.sortingLayerName = m_sortingLayerName;
+        m_sprite.sortingOrder = m_sortingLayerOrder;
+        m_teleported = true;
+
     }
 }
